9f5dfe969197bd70d94f81d858ecc1547df9644d
Chris Bubel <christopher.s.bubel@gmail.com>
2017-01-10 11:00:21 -0500

    Attempt to fix abstraction

--- js/router.js
+++ js/router.js
@@ -1,29 +1,32 @@
 const HOME_PARTIAL = "home"; // home partial filename
+const CONTAINER_ID = "content";
 
 function load() {
     let path = getPath();
     if(path === "" || path === undefined) path = HOME_PARTIAL;
 
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
         if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("content").innerHTML =
-        this.responseText;
+            document.getElementById(CONTAINER_ID).innerHTML = this.responseText;
         }
     };
+
     xhttp.open("GET", `partials/${path}.html`, true);
     xhttp.send();
 }
+
+
 
 function getPath() {
     return window.location.href.split("#/")[1];
 }
 
-function loadHandler(event) {
+let loadHandler = function(event) {
     event.preventDefault();
     load();
 }
 
-window.onhashchange = (event) => loadHandler(event);
+window.onhashchange = event => loadHandler(event);
 
-window.onload = (event) => loadHandler(event);
+window.onload = event => loadHandler(event);
